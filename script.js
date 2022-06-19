44e1c4a39090c94b68183d9783846ec3407e1972
amaury <tissot.amaury@gmail.com>
2022-06-19 17:19:24 +0200

    Add constructor

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// QuerySelector variables
+
 const btn = document.querySelector('.add-button');
 const modal = document.querySelector('.modal');
 const closeSpan = document.querySelector('#close');
@@ -6,6 +8,22 @@ const trash = document.querySelector('.trash-icon');
 const form = document.querySelector('form');
 const submit = document.querySelector('.submit');
 const getTitle = document.querySelector('#get-title');
+const getAuthor = document.querySelector('#get-author');
+const getNumberOfPages = document.querySelector('#get-pages');
+const getReading = document.querySelector('#get-read');
+
+// Data variables
+
+let titleInfo;
+let authorInfo;
+let numberOfPagesInfo;
+let readingInfo;
+let idNumber = 0;
+
+// Array
+
+let myLibrary = []
+
 // Function who open the modal popup
 
 const btnAdd = () => {
@@ -33,27 +51,69 @@ const windowCloseModal = () => {
 const newBookDisplay = (newId) => {
     const divClone = divBookList.cloneNode(true);
     divClone.id = newId;
+    divClone.querySelector('.title-display').textContent = titleInfo;
+    divClone.querySelector('.author-display').textContent = authorInfo;
+    divClone.querySelector('.number-of-pages-display').textContent = numberOfPagesInfo;
+    divClone.querySelector('.reading-status-display').textContent = readingInfo;
+    divClone.querySelector('.trash-icon').id = newId;
     document.body.appendChild(divClone);
-    divBookList.style.display = "grid";
+    //divBookList.style.display = "grid";
     divClone.style.display = "grid";
 }
 
 // Function who delete the new book-list
 
 const deleteBookList = () => {
-    trash.addEventListener('click', () => {
-        divBookList.style.display = "none";
-        divClone.style.display= "none";
-})};
+    trash.addEventListener('click', (event) => 
+    console.log(event.target.parentElement.id))
+        
+};
 
 
-// Function listener submit button
+// Function listener submit button + get form value
 
 const submitBtn = () =>{
     submit.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log(getTitle.value)})
-};
+        titleInfo = getTitle.value;
+        authorInfo = getAuthor.value;
+        numberOfPagesInfo = getNumberOfPages.value;
+        readingInfo = getReading.value;
+        //console.log(titleInfo + authorInfo + numberOfPagesInfo + readingInfo)
+        addBookToLibrary();
+        console.table(myLibrary);
+        modal.style.display = "none";
+        cleanFormInput();
+        newBookDisplay(idNumber);
+        idNumber ++;
+        deleteBookList();
+
+    })};
+
+
+// Book list constructor
+
+function Book(title, author, numberOfPages, readingStatus) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.readingStatus = readingStatus;
+}
+
+// Function who add book info to the library array
+
+function addBookToLibrary () {
+    let newBook = new Book(titleInfo, authorInfo, numberOfPagesInfo, readingInfo);
+    myLibrary.push(newBook);
+}
+
+// Function who "clean" the form input;
+
+function cleanFormInput () {
+    getTitle.value = '';
+    getAuthor.value = '';
+    getNumberOfPages.value = '';
+}
 
 
 // Main function
@@ -64,7 +124,7 @@ const main = () => {
     windowCloseModal();
     deleteBookList();
     submitBtn();
-    //newBookDisplay(1);
 }
 
 main();
+
